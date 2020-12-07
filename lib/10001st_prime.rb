449445aabbeb2ed17433a8e24cad821ccd083e2d
bahayg <bahayg@gmail.com>
2020-12-06 16:56:12 -0800

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,26 @@
 # Implement your procedural solution here!
+class Prime
+    attr_reader :number, :nth_element
+
+    def initialize(nth_element)
+        @nth_element = nth_element
+        @number = prime_number_for(nth_element)
+    end
+
+    def is_prime?(number)
+        return false if(number.zero?) || number == 1
+        integers =* (2..Math.sqrt(number).round)
+        integers.each {|i| return false if(number % i).zero?}
+        true
+    end
+    
+    def prime_number_for(nth_element)
+        count = 0
+        number = 0
+        while count != nth_element
+            number += 1
+            count += 1 if is_prime?(number)
+        end
+        number
+    end 
+end
